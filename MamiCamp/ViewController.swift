aa84da5ba1bc008b746e748cc6206ffd3fe98b44
icungse <icungse@gmail.com.com>
2019-03-05 17:41:20 +0700

    Initial Commit

---
+++ MamiCamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MamiCamp
+//
+//  Created by icungse on 05/03/19.
+//  Copyright © 2019 icung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
